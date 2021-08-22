b2707c3ff2e3f2bcce9b9eb294aff6fe5793d663
ImGabreuw <imgabreuw.dev@gmail.com>
2021-08-22 16:38:03 -0300

    First commit

---
+++ src/main/kotlin/me/gabreuw/avengersapi/AvengersApiApplication.kt
@@ -0,0 +1,11 @@
+package me.gabreuw.avengersapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AvengersApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<AvengersApiApplication>(*args)
+}
